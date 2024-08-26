ec700bd4fee25b341bb9caf04f376c2bc761d4a4
NunoMore <nunomoreira16@gmail.com>
2024-08-25 21:23:37 -0500

    improve platform verification

--- app/profile/profile-vouchers.tsx
+++ app/profile/profile-vouchers.tsx
@@ -4,20 +4,21 @@ import { ThemedView } from "@/components/ThemedView";
 import { Screens } from "@/constants/Screens";
 import { sendAlert } from "@/utils/utils";
 import { router } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 
 const ProfileVouchers = () => {
+  const handleEditVoucher = () => router.navigate(Screens.VouchersEdit);
+  const handleVerifyVoucher = () => {
+    if (Platform.OS !== "ios" && Platform.OS !== "android")
+      sendAlert("Verify Voucher", "Not available in web");
+    else router.navigate(Screens.VouchersRead);
+  };
+
   return (
     <ThemedView style={styles.container}>
       <View style={styles.buttonsContainer}>
-        <SquareButton
-          text="Edit Vouchers"
-          onPress={() => router.navigate(Screens.VouchersEdit)}
-        />
-        <SquareButton
-          text="Verify Voucher"
-          onPress={() => router.navigate(Screens.VouchersRead)}
-        />
+        <SquareButton text="Edit Vouchers" onPress={handleEditVoucher} />
+        <SquareButton text="Verify Voucher" onPress={handleVerifyVoucher} />
       </View>
     </ThemedView>
   );
